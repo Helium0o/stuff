6b4ebb9444e46197944e6a6d6163b3da559a11c2
Helium0o <70913369+Helium0o@users.noreply.github.com>
2024-01-25 14:59:16 +0000

    Update Organize.py

--- Organize.py
+++ Organize.py
@@ -1,4 +1,4 @@
-def run_macro(macro_name):
+def run_macro(macro_names):
     notepad.runMenuCommand('Macro', '1 Numbers remain')
     notepad.runMenuCommand('Macro', '2 Remove spaces')
     notepad.runMenuCommand('Macro', '3 Add quotes')
@@ -7,6 +7,7 @@ def run_macro(macro_name):
     editor = notepad.getCurrentEditor()
     editor.addText(macro_name)
     editor.clearAll()
-
+macro_names = ['1 Numbers remain', '2 Remove spaces', '3 Add quotes', '4 Pretty', '5 Add 20000']
+# Run each macro
 for macro_name in macro_names:
     run_macro(macro_name)
